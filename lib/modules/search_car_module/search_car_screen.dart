0096d2d5f686bcc49d92c9462e22d4d12eec9ff3
HOSSAMSAFAFA <hossamsafafa32@gmail.com>
2024-08-02 22:26:22 +0300

    search for a car

--- lib/modules/search_car_module/search_car_screen.dart
+++ lib/modules/search_car_module/search_car_screen.dart
@@ -20,7 +20,6 @@ class _SearchCarScreenState extends State<SearchCarScreen> {
   Widget _widgetToShow = Container();
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
     return Container(
       color: backGroundColor,
       width: MediaQuery.of(context).size.width * .75,
@@ -74,50 +73,6 @@ class _SearchCarScreenState extends State<SearchCarScreen> {
                               width: 2,
                               color: mainColor, // The color for the bottom border
                             ),
-=======
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          TextFormField(
-            controller: _controller,
-            decoration: InputDecoration(
-              hintText: "Car Number",
-              enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                borderSide: BorderSide(color: mainColor),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                borderSide: BorderSide(color: mainColor),
-              ),
-              errorBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                borderSide:
-                    BorderSide(color: const Color.fromARGB(255, 172, 13, 2)),
-              ),
-              counterText: '', // Hide the counter text
-              prefixIcon: Padding(
-                padding: EdgeInsets.all(
-                    10.0), // Add padding to align segments properly
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: List.generate(8, (index) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width * 0.02,
-                      alignment: Alignment.center,
-                      child: Text(
-                        _controller.text.length > index
-                            ? _controller.text[index]
-                            : '',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border(
-                          bottom: BorderSide(
-                            width: 2,
-                            color: mainColor, // The color for the bottom border
->>>>>>> 4a2ef3ac6572f75df1bc1d0ea8d4455368f1ccd9
                           ),
                         ),
                       );
@@ -125,82 +80,12 @@ class _SearchCarScreenState extends State<SearchCarScreen> {
                   ),
                 ),
               ),
-<<<<<<< HEAD
               inputFormatters: [
                 LengthLimitingTextInputFormatter(8), // Limit to 6 digits
               ],
               validator: (value) {
                 if (value == null || value.isEmpty) {
                   return 'Please enter mileage';
-=======
-            ),
-            inputFormatters: [
-              LengthLimitingTextInputFormatter(8), // Limit to 6 digits
-            ],
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter mileage';
-              }
-              return null;
-            },
-            onChanged: (value) {
-              // Trigger a rebuild to update the segmented display
-              (context as Element).markNeedsBuild();
-            },
-          ),
-          SizedBox(height: 16.0),
-          Container(
-            color: Colors.red,
-            child: MaterialButton(
-              child: Text("Search"),
-              onPressed: () async {
-                String carNumber = "";
-                carNumber = addSpaceBetweenEachLetter(_controller.text);
-                final dbHelper = DatabaseHelper();
-                CarModel? car = await dbHelper.getCarByNumber(carNumber);
-                if (car == null) {
-                  setState(() {
-                    _widgetToShow = Container(
-                      child: Center(
-                        child: Text(
-                          'Car not found',
-                          style: TextStyle(color: Colors.grey, fontSize: 40),
-                        ),
-                      ),
-                    );
-                  });
-                } else {
-                  List<ServiceModel> services =
-                      await dbHelper.getServicesByCarNumber(carNumber);
-                  setState(() {
-                    _widgetToShow = Container(
-                      padding: EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Column(
-                        children: [
-                          SizedBox(height: 16.0),
-                          Text("Services"),
-                          SizedBox(height: 16.0),
-                          Expanded(
-                            child: ListView.separated(
-                              itemBuilder: (context, index) => buildservice(
-                                services[index],
-                              ),
-                              separatorBuilder: (context, index) => SizedBox(
-                                height:
-                                    MediaQuery.of(context).size.height * .01,
-                              ),
-                              itemCount: services.length,
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  });
->>>>>>> 4a2ef3ac6572f75df1bc1d0ea8d4455368f1ccd9
                 }
                 return null;
               },
@@ -301,6 +186,7 @@ class _SearchCarScreenState extends State<SearchCarScreen> {
         ),
       ),
     );
+  ;
   }
 
   Widget buildservice(ServiceModel service) {
